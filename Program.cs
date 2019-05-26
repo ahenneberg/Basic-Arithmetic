11322caf70d76f5377d421b5d9352f79f837ed72
ahenneberg <51038369+ahenneberg@users.noreply.github.com>
2019-05-25 22:58:00 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;                           // Importing Namespace
+
+namespace A_First_CSharp_Program        // Namespace Declaration
+{
+    class Program                       // Class Declaration
+    {
+        static void Main()              // Method Declaration
+        {
+            int x = 12 * 30;            // Statement 1
+            Console.WriteLine(x);       // Statement 2
+        }                               // End of Method
+    }                                   // End of Class
+}                                       // End of Namespace
